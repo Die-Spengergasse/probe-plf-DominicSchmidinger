38cff2601486e6a82020e8dba2722301736bb6a6
Kas240614 <Kas240614@plf.spengergasse.at>
2025-10-20 15:46:31 +0200

    autocommit

--- bluej_projekt/Array.java
+++ bluej_projekt/Array.java
@@ -12,7 +12,7 @@ public class Array {
     public static int arraySum(int[] arr) {
         int rv = 0;
         for(int i = 0; i  < arr.length; i++){
-        
+
             rv += arr[i];
         }
         return rv;
@@ -21,10 +21,11 @@ public class Array {
     // hier wird das größte Element des Arrays gesucht
     public static int arrayMax(int[] arr) {
         int rv = Integer.MIN_VALUE;
-        
+        for (int i = 0; i < arr.length; i++){
+            
+        }
         return rv;
     }
-
     // hier wird das kleinste Element des Arrays gesucht
     public static int arrayMin(int[] arr) {
         int rv = Integer.MAX_VALUE;
